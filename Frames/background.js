7ba5a02491a10f726ba2214c488e2f34b9acdce8
Ralph Bruens <32263203+Ralphbruens@users.noreply.github.com>
2020-04-15 17:58:28 +0200

    Add files via upload

---
+++ Frames/background.js
@@ -0,0 +1,21 @@
+// function to calculate the result of the survey
+
+
+$( window ).ready(function() {
+
+    var wHeight = $(window).height();
+
+    $('.slide')
+      .height(wHeight)
+      .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+
+          var bgColor = elem.data('background');
+
+          $('body').css('background-color', bgColor);
+
+        }
+      });
+
+  });
