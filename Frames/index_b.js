fa0f7c9837dcdc0ab776184bbcf3f7af1049d2de
Ralph Bruens <Ralphbruens@hotmail.com>
2020-04-29 15:23:42 +0200

    FREAM B UPDATE

---
+++ Frames/index_b.js
@@ -0,0 +1,19 @@
+$(".icon").click(function(){
+  $(".image04").fadeToggle();
+});
+
+$(".icon2").click(function(){
+  $(".image05").fadeToggle();
+});
+
+$(".image04").click(function(){
+  $(".ui-widget-content").fadeToggle();
+});
+
+$(".image05").click(function(){
+  $(".image08").fadeToggle();
+});
+
+$(".image08").click(function(){
+  $(".image09").fadeToggle();
+});
