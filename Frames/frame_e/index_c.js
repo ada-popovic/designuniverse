53d163cfa7140098afda1337912458c6d99c5bb8
ada popovic <popowucz.adrianna@gmail.com>
2020-04-30 18:19:11 +0200

    adding c e d

---
+++ Frames/frame_e/index_c.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() {
+  var $art = $("#art");
+  var $blocks = $("#block");
+  var $lis = $("#block li");
+  var $control = $("#control1");
+  var $inputs = $(".control input");
+  var $output = $("output");
+
+  $control.on("keyup", ".text-data", updateText)
+
+  function updateText(){
+    var val = $(this).val();
+    if (val == "") {
+      val = "YOUR TEXT HERE";
+    }
+    val = val.split(" ").join("&nbsp;&nbsp;");
+    $lis.html(val);
+  }
+});
+
+
+javascript:document.body.contentEditable='true'; document.designMode='on';
+
+
+function setFontText(text) {
+       document.getElementById("Courier_new").innerHTML = text;
+       document.getElementById("Arial_Black").innerHTML = text;
+}
