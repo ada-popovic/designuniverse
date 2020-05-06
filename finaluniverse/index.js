2e1abbce8425d7c71e0c476e7b2cdcaca764eff8
Ralph Bruens <Ralphbruens@hotmail.com>
2020-05-06 15:28:10 +0200

    update

---
+++ finaluniverse/index.js
@@ -0,0 +1,33 @@
+// UNIVERSE 1
+
+$(".u1-transport-u2").click(function(){
+  $(".index-universe2").fadeToggle();
+});
+
+$(".u1-transport-u4").click(function(){
+  $(".index-universe4").fadeToggle();
+});
+
+// UNIVERSE 2
+
+$(".u2-transport-u3").click(function(){
+  $(".index-universe3").fadeToggle();
+});
+
+// UNIVERSE 3
+
+$(".u3-transport-u4").click(function(){
+  $(".index-universe4").fadeToggle();
+});
+
+// UNIVERSE 4
+
+$(".u4-transport-u5").click(function(){
+  $(".index-universe5").fadeToggle();
+});
+
+// UNIVERSE 5
+
+$(".u5-transport-print").click(function(){
+  $(".index-printbutton").fadeToggle();
+});
