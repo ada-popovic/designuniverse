ef75d64b823bc81043decc7184de9884a2ed67a4
Ralph Bruens <Ralphbruens@hotmail.com>
2020-06-02 16:20:31 +0200

    Update Universe 4
    Added Images to universe four, different click events + draggable

--- finaluniverse/U4/U4.js
+++ finaluniverse/U4/U4.js
@@ -1,1 +1,38 @@
+document.getElementById("u4-banana").addEventListener("click", rainfall);
 
+function rainfall() {
+
+$(document).ready(function(){
+
+      for (var i = 0; i < 20; i++) {
+
+          var div = $("<div>");
+          div.addClass("rainfall");
+
+          var img = "<img src ='U4/img/41&46_APVR.png' id='rainfall"+i+"'/>";
+          div.append(img);
+
+          var bodyWidth = document.body.clientWidth;
+          var bodyHeight = document.body.clientHeight;
+          var randPosX = Math.floor((Math.random()*bodyWidth*1.2));
+          var randPosY = Math.floor((Math.random()*bodyHeight*1.5));
+
+          div.css('left', randPosX);
+          div.css('top', randPosY);
+
+          $("body").append(div);
+      }
+
+})
+
+}
+
+$( function() {
+  $( '.u4-eye' ).draggable( { });
+$( '.u4-eye-2' ).draggable( { });
+$( '.u4-banana' ).draggable( { });
+$( '.u4-jazz' ).draggable( { });
+$( '.u4-font' ).draggable( { });
+$( '.u4-bas' ).draggable( { });
+$( '.u4-family' ).draggable( { });
+});
