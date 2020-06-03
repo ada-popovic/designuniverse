4a9f6f9104df0e521df343f80fca15a5a9eb24d9
ada popovic <popowucz.adrianna@gmail.com>
2020-06-03 09:58:05 +0200

    --

--- finaluniverse/U5/U5.js
+++ finaluniverse/U5/U5.js
@@ -1,3 +1,14 @@
+$( function() {
+
+  $( '.u5-tea' ).draggable( { });
+    $( '.u5-guma' ).draggable( { });
+      $( '.u5-omlet' ).draggable( { });
+        $( '.u5-tost' ).draggable( { });
+          $( '.u5-banan' ).draggable( { });
+            $( '.u5-chrupki' ).draggable( { });
+              $( '.u5-paint' ).draggable( { });
+              $( '.u5-paprykarz' ).draggable( { });
+});
 
 $(".u5-text-data-1").click(function(){
   // $(this).css("background-color", "red");
@@ -13,3 +24,9 @@ function textAreaAdjust(o) {
   o.style.height = "1px";
   o.style.height = (25+o.scrollHeight)+"px";
 }
+
+
+$(".u5-paint").click(function(){
+  // $(this).css("background-color", "red");
+  $(".u5-paint").removeClass("no-print");
+});
